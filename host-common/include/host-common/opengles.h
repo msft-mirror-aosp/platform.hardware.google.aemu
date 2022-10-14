miyu-xu/hardware-google-aemu

Add graphics process creation callback
... so that the renderer will know the correct time to create process
resources instead of having to create the reource lazily.

Change-Id: Ie4107a78ef7581273a80580e6c29309fe0a891ce

host-common/include/host-common/opengles.h
@@ -132,6 +132,9 @@ AEMU_EXPORT void android_finishOpenglesRenderer();
  */
 AEMU_EXPORT extern int  android_gles_fast_pipes;
 
+// Notify the renderer that a guest graphics process is created or destroyed.
+AEMU_EXPORT void android_onGraphicsProcessCreate(uint64_t puid);
+// TODO(kaiyili): rename this API to android_onGraphicsProcessDestroy
 AEMU_EXPORT void android_cleanupProcGLObjects(uint64_t puid);
 
 AEMU_EXPORT void android_waitForOpenglesProcessCleanup();

host-common/opengl/GLProcessPipe.cpp
@@ -98,6 +98,7 @@ public:
         }
         AutoLock lock(sRegistry.lock);
         sRegistry.ids.insert(m_uniqueId);
+        android_onGraphicsProcessCreate(m_uniqueId);
     }
 
     ~GLProcessPipe() {

host-common/opengles.cpp
@@ -547,6 +547,12 @@ const emugl::RendererPtr& android_getOpenglesRenderer() {
     return sRenderer;
 }
 
+void android_onGraphicsProcessCreate(uint64_t puid) {
+    if (sRenderer) {
+        sRenderer->onGraphicsProcessCreate(puid);
+    }
+}
+
 void android_cleanupProcGLObjects(uint64_t puid) {
     if (sRenderer) {
         sRenderer->cleanupProcGLObjects(puid);

msft-mirror-aosp/platform.hardware.google.aemu

host-common/include/host-common/opengles.h
@@ -132,6 +132,9 @@ AEMU_EXPORT void android_finishOpenglesRenderer();
  */
 AEMU_EXPORT extern int  android_gles_fast_pipes;
 
+// Notify the renderer that a guest graphics process is created or destroyed.
+AEMU_EXPORT void android_onGuestGraphicsProcessCreate(uint64_t puid);
+// TODO(kaiyili): rename this API to android_onGuestGraphicsProcessDestroy
 AEMU_EXPORT void android_cleanupProcGLObjects(uint64_t puid);
 
 AEMU_EXPORT void android_waitForOpenglesProcessCleanup();

host-common/opengl/GLProcessPipe.cpp
@@ -98,6 +98,7 @@ public:
         }
         AutoLock lock(sRegistry.lock);
         sRegistry.ids.insert(m_uniqueId);
+        android_onGuestGraphicsProcessCreate(m_uniqueId);
     }
 
     ~GLProcessPipe() {

host-common/opengles.cpp
@@ -547,6 +547,12 @@ const emugl::RendererPtr& android_getOpenglesRenderer() {
     return sRenderer;
 }
 
+void android_onGuestGraphicsProcessCreate(uint64_t puid) {
+    if (sRenderer) {
+        sRenderer->onGuestGraphicsProcessCreate(puid);
+    }
+}
+
 void android_cleanupProcGLObjects(uint64_t puid) {
     if (sRenderer) {
         sRenderer->cleanupProcGLObjects(puid);